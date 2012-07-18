64f72e6a422c951926b6aa921c27bcd670efff67
David Julia <dajulia3@gmail.com>
2012-07-18 16:19:53 -0600

    Added hooks to output ga post/page event-tracking javascript

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Yoast Extension: User views Google Analytics Tracking
+Plugin Name: Yoast Extension: User Views Google Analytics Tracking
 Plugin URI: http://davidjulia.wordpress.com
 Description: Allows you to run yoast plugin and have it push google analytics data on localhost
 Version: .01
@@ -8,6 +8,41 @@ Author: David Julia
 Author URI: http://davidjulia.wordpress.com
 License: GPL2
 */
+$DEVELOPMENT_ENV = true;
 
-$js_code = "_gaq.push(['_setDomainName', 'none']);";
-$push = apply_filters('yoast-ga-push-before-pageview',$js_code);
+if($DEVELOPMENT_ENV){
+	$local_dev_js = "_gaq.push(['_setDomainName', 'none']);";
+	$push = apply_filters('yoast-ga-push-before-pageview', $local_dev_js);
+	
+}
+
+add_action('wp_head', 'daj_gax_add_event_tracking_js');
+
+function daj_gax_add_event_tracking_js(){
+	if($_GET['preview']==true) //Don't track previews
+	{
+		return;
+	}
+	if($_GET['p']) //viewing a post
+	{
+		$post_name = get_the_title($_GET['p']);
+		echo "_gaq.push(['_trackEvent', 'Posts', 'View', ${$post_name}]);\n";
+		
+	}
+	else if($_GET['page_id']) //viewing a page
+	{
+		$page_name = get_the_title($_GET['page_id']);
+		echo "_gaq.push(['_trackEvent', 'Pages', 'View', ${$page_name}]);\n";
+	}
+	
+}
+
+// if($post_type == 'post' || $post_type == 'page')
+// {
+	//$post_name_js = "_gaq.push(['_setCustomVar', 2, 'Post Name', '${"boo"}', 3]);  ";	
+// }
+//Set the Post Name custom var as a page-level scoped (3) var to index 2
+
+	//$push = apply_filters('yoast-ga-push-before-pageview', $post_name_js);
+
+	
