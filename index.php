99e28286393e9ea3735f0d3c3068078a7d98e280
David Julia <dajulia3@gmail.com>
2012-07-23 19:52:31 -0600

    Added setAccount before trackEvent call so that the events will actually get tracked

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Author URI: http://davidjulia.wordpress.com
 License: GPL2
 */
 $DEVELOPMENT_ENV = true;
-
+$ua_code = "UA-33319912-1";
 if($DEVELOPMENT_ENV){
 	$local_dev_js = "_gaq.push(['_setDomainName', 'none']);";
 	$push = apply_filters('yoast-ga-push-before-pageview', $local_dev_js);
@@ -26,6 +26,8 @@ function daj_gax_add_event_tracking_js(){
 	
 	echo '<script type="text/javascript">';
 	echo "\nvar _gaq = _gaq || [];\n"; //ensure the gaq is there, or make it
+	echo "_gaq.push(['_setAccount','${ua_code}'])";
+
 	if($_GET['p']) //viewing a post
 	{
 		$post_name = get_the_title($_GET['p']);
@@ -41,12 +43,29 @@ function daj_gax_add_event_tracking_js(){
 	echo '</script>';
 }
 
-// if($post_type == 'post' || $post_type == 'page')
-// {
-	//$post_name_js = "_gaq.push(['_setCustomVar', 2, 'Post Name', '${"boo"}', 3]);  ";	
-// }
-//Set the Post Name custom var as a page-level scoped (3) var to index 2
+// function daj_gax_output_event_tracking_js(){
+// 	if($_GET['preview']==true) //Don't track previews
+// 	{
+// 		return "";
+// 	}
 
-	//$push = apply_filters('yoast-ga-push-before-pageview', $post_name_js);
+// 	//echo '<script type="text/javascript">';
+// 	$ga_js = "\nvar _gaq = _gaq || [];\n"; //ensure the gaq is there, or make it
+// 	if($_GET['p']) //viewing a post
+// 	{
+// 		$post_name = get_the_title($_GET['p']);
+// 		$ga_js.= "_gaq.push(['_trackEvent', 'Posts', 'View', '${post_name}']);\n";
+
+// 	}
+// 	else if($_GET['page_id']) //viewing a page
+// 	{
+// 		$page_name = get_the_title($_GET['page_id']);
+// 		$ga_js.= "_gaq.push(['_trackEvent', 'Pages', 'View', '${page_name}']);\n";
+// 	}
+
+// 	return $ga_js;
+// }
+
+// 	$push = apply_filters('yoast-ga-push-before-pageview', daj_gax_output_event_tracking_js());
 
 	
